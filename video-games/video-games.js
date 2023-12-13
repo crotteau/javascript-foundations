905ef6dec5df8a466ab49222940444428c3d32d6
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-12 17:52:03 -0700

    completed test functions

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,40 @@
+function createPlayer(name, age, moves) {
+    var player = {
+        name: name,
+        age: age,
+        moveset: moves
+    }; return player
+}
 
+function createLevel(name, newPlayer) {
+    var level = {
+        name: name,
+        players: newPlayer,
+        coins: 0,
+        lives: 3
+    }; return level
+}
 
+function findCoins(level, coinNumber) {
+    level.coins = coinNumber
+    if (coinNumber >= 100) {
+        level.lives ++
+    }
+    return level
+} 
 
-module.exports = { };
+function defeatPlayer(level) {
+    level.lives --
+    if (level.lives === 0) {
+        return 'GAME OVER'
+    } else { 
+        return level 
+    }
+}
+
+module.exports = {
+    createPlayer,
+    createLevel,
+    findCoins,
+    defeatPlayer
+ };
