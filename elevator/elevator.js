0c96911eff143fa82712b67ca1d333f770e0252c
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-13 08:52:03 -0700

    Completed test functions

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,35 @@
 
+function createElevator(building, floors, currentFloor, passengers) {
+    var newElevator = {
+        building: building,
+        floors: floors,
+        currentFloor: currentFloor,
+        passengers: passengers
+    }
+    return newElevator
+}
 
-module.exports = {  };
+function changeFloors(elevator, floor) {
+    if (elevator.floors < floor) {
+        return `Floor ${floor} does not exist!`
+    }
+    else if (elevator.currentFloor === floor) {
+        return `You're already on floor ${floor}!`
+    } else {
+        return `Taking you to floor ${floor}!`  
+    }
+}
+
+// input: strings of elevator and passenger name
+// output: an array of passengers minus passenger name
+// using splice
+function dropOffPassenger(elevator, passenger) {
+    var passengerIndex = elevator.passengers.indexOf(passenger)
+    elevator.passengers.splice(passengerIndex, 1)
+    return elevator.passengers
+}
+module.exports = { 
+    createElevator,
+    changeFloors,
+    dropOffPassenger
+ };
