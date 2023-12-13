f31867af1c54038d3f54765b695a8b88be65a48a
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-12 19:33:41 -0700

    Working test functions

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,30 @@
+function createMeal(meal, calories) {
+    var mealType = {
+        type: meal,
+        calorieGoal: calories,
+        dishes: []
+    }
+    return mealType
+}
 
-module.exports = { }
+function addDish(meal, newDish) {
+    if (newDish.calories <= meal.calorieGoal)
+        meal.dishes.push(newDish)
+        meal.calorieGoal -= newDish.calories
+     return meal
+}
+
+function calculateCalories(entireMeal) {
+    var totalCalories = 0
+    for (var i = 0; i < entireMeal.dishes.length; i++) {
+        var calories = entireMeal.dishes[i].calories
+        totalCalories += calories
+    } 
+        return `${entireMeal.type} has a total of ${totalCalories} calories.`
+}
+
+module.exports = { 
+    createMeal,
+    addDish,
+    calculateCalories
+}
