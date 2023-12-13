e3c5d5be26b2fc75800b9d66c21f2cdcf711a5bb
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-12 18:57:00 -0700

    Completed test functions

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,42 @@
 
-module.exports = {}
+function createIngredient(ingredient, price) {
+    var newIngredient = {
+        name: ingredient || 'unknown',
+        price: price || 0.00
+    }
+     return newIngredient
+} 
+
+// createTaco
+function createTaco(tacoName, tacoIngredient) {
+    var newTaco = {
+        name: tacoName || 'custom',
+        ingredients: tacoIngredient || []
+    }
+ return newTaco
+} 
+
+function addIngredientToTaco(addToTaco, addIngredient) {
+    var updatedTaco = {
+        name: addToTaco.name,
+        ingredients: addToTaco.ingredients
+    } 
+    updatedTaco.ingredients.push(addIngredient)
+    return updatedTaco
+}
+
+function calculatePrice(tacoType) {
+ var totalPrice = 0
+    for (var i = 0; i < tacoType.ingredients.length; i++) {
+         var tacoPrice = tacoType.ingredients[i].price
+         totalPrice += tacoPrice 
+    }
+     return totalPrice
+} 
+
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice
+}
