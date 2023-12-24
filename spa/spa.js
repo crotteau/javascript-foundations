17ac1e169f858586b03d090384664a24c9720c7d
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-24 12:01:19 -0700

    Working test functions

--- spa/spa.js
+++ spa/spa.js
@@ -1,8 +1,46 @@
+function createCustomer(name, bill, bookings) {
+  var customer = {
+    name: name,
+    bill: bill,
+    bookings: bookings || []
+  }
+  return customer
+}
 
+function greeting(customerName) {
+  if (customerName.bookings.length === 0) {
+    return `${customerName.name}! Welcome to Happy Spa`
+  } else {
+    return `${customerName.name}! Welcome back to Happy Spa`
+  }
+}
+
+function createService(serviceName, cost) {
+  if (serviceName === undefined || cost === undefined) {
+  return 'Please provide service name and cost.'
+} else { return {name:serviceName, cost:cost}
+}
+}
+
+function bookServices(customerName, serviceName) {
+  customer = customerName
+  customer.bookings.push(serviceName.name)
+  customer.bill += serviceName.cost
+  return customer
+}
+
+function applyGiftCard(allServices, giftCard) {
+  var affordableServices = []
+  for (var i = 0; i < allServices.length; i++) {
+    if (allServices[i].price <= giftCard) {
+      affordableServices.push(allServices[i].name)
+    }
+  } return affordableServices
+}
 module.exports = { 
-  // createCustomer, 
-  // greeting, 
-  // createService,
-  // bookServices, 
-  // applyGiftCard 
+  createCustomer, 
+  greeting, 
+  createService,
+  bookServices, 
+  applyGiftCard 
 }
