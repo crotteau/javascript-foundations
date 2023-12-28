e6d7389d71a040d2ce6d3b4820706a630478b3a1
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-27 21:09:24 -0700

    Working test functions

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,34 @@
+function createBarber(name, earnings, haircuts) {
+    var barber = {
+        name: name,
+        earnings: earnings || 0,
+        haircuts: haircuts || []
+    }
+    return barber
+}
 
+function giveCompliment(haircut) {
+    return `This ${haircut.style} looks great!`
+}
 
-module.exports = {  };
+function cutHair(barber, haircut) {
+    barber.haircuts.push(haircut)
+    barber.earnings += haircut.price
+    return barber
+}
+
+function listStyles(barber, length) {
+    var styles = []
+    for (var i = 0; i < barber.haircuts.length; i++) {
+        if (barber.haircuts[i].hairLength === length) {
+            styles.push(barber.haircuts[i].style)
+        }
+    }
+    return styles
+}
+module.exports = {
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+ };
