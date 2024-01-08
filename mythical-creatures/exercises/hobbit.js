35a4a1d4567d35ca75ddc9580f479ac041fb5bd7
Megan Crotteau <megan.crotteau@gmail.com>
2024-01-08 08:42:46 -0700

    Completed test functions

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,9 +1,51 @@
+// createHobbit
+var acquaintances = []
+function createHobbit(name, age) {
+  var hobbit = {
+    name: name,
+    age: age,
+    isAdult: age >= 33,
+    isOld: age >= 101,
+    acquaintances: acquaintances
+  }; if (age === undefined && name === undefined) {
+      hobbit.age = 0
+      hobbit.name = 'unknown'
+        return hobbit
+  } else if (name === undefined) {
+      hobbit.name = 'unknown'
+       return hobbit
+  } else if (age === undefined) {
+      hobbit.age = 0
+       return hobbit
+  } else hobbit.age = age
+       return hobbit
+} 
 
+// celebrateBirthday
+function celebrateBirthday(hobbit) {
+  hobbit.age ++
+    hobbit.isAdult = (hobbit.age >= 33)
+    hobbit.isOld = (hobbit.age >= 101)
+      return hobbit
+}
+
+// getRing
+function getRing(hobbit) {
+  if (hobbit.name === 'Frodo') {
+    return 'Here is the ring!'
+  } else return 'You can\'t have it!'
+}
+
+// meetPeople
+function meetPeople(hobbit, people) {
+  hobbit.acquaintances.push(people)
+  return hobbit
+}
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
-  // getRing, 
-  // meetPeople, 
+  createHobbit, 
+  celebrateBirthday, 
+  getRing, 
+  meetPeople, 
   // findFriends
 }
--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,32 @@
+function createItemStock(name, quantity, price) {
+    var item = {
+        name: name || 'unknown',
+        quantity: quantity || 0, 
+        price: price || 1.00
+    }
+    return item
+}
 
-module.exports = {}
+function makePurchase(selectedItem, moneyForPurchase) {
+    if (selectedItem.price > moneyForPurchase) {
+        return `Sorry, you need at least $${selectedItem.price} to make that purchase`
+    } else if (selectedItem.quantity === 0) {
+        return 'Sorry, there are none left'
+    } else {
+        return `Here are your ${selectedItem.name}`
+    }
+}
+
+function collectChange(looseChange) {
+    var totalChange = 0
+    for (var i = 0; i < looseChange.length; i++) {
+       totalChange += looseChange[i]
+    }
+    return totalChange
+}
+
+module.exports = {
+    createItemStock,
+    makePurchase,
+    collectChange
+}
