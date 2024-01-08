5fc1b743e52015cddc4eed25369cf352f0859f52
Megan Crotteau <megan.crotteau@gmail.com>
2024-01-08 16:08:02 -0700

    Completed test functions

--- vhs/vhs.js
+++ vhs/vhs.js
@@ -1,3 +1,37 @@
+function createTape(title, readyToPlay) {
+    var tape = {
+        title: title,
+        readyToPlay: readyToPlay || false
+    }
+    return tape
+}
 
+function reset(tape) {
+    tape.readyToPlay = true
+    return tape
+}
 
-module.exports = {  }
+function createCollection(tape1, tape2, tape3) {
+    var collection = []
+    if (!tape1 && !tape2 && !tape3) {
+        return 'Your collection is empty.'
+    } else {
+        collection.push(tape1, tape2, tape3)
+        return collection
+    }
+}
+
+function previewCollection(collection) {
+    var preview = []
+    for (var i = 0; i < collection.length; i++) {
+        preview.push(collection[i].title)
+    }
+    return preview
+}
+
+module.exports = { 
+    createTape,
+    reset,
+    createCollection,
+    previewCollection
+ }
