3b34369567bbbf9e19df9538d739e47c5d2225c3
Megan Crotteau <megan.crotteau@gmail.com>
2024-01-08 16:36:54 -0700

    Completed test functions

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,37 @@
+function createFavoriteFood(food) {
+    var favFood = {
+        name: food.dish,
+        ingredients: food.ingredients,
+        isSpicy: food.isSpicy,
+        timesOrdered: 0
+    }
+    return favFood
+}
 
+function commentOnSpiciness(dish) {
+    if (dish.isSpicy) {
+        return `Wow, this ${dish.name} is so spicy!`
+    } else return `Phew, this ${dish.name} is not very spicy.`
+}
 
+function orderFood(dish) {
+    dish.timesOrdered += 1
+    return dish
+}
 
-module.exports = { };
+function createShoppingList(dishes) {
+    var shoppingList = []
+    for (var i = 0; i < dishes.length; i++) {
+        for (var i = 0; i < dishes[i].length; i++) {
+            shoppingList.push(dishes.ingredients[i])
+        }
+    }
+    return shoppingList
+}
+
+module.exports = { 
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList
+};
