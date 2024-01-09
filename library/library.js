b2e4cf8af7162c74ace1ab85a127d1f3f1c16af2
Megan Crotteau <megan.crotteau@gmail.com>
2024-01-08 22:09:54 -0700

    Working test functions

--- library/library.js
+++ library/library.js
@@ -1,3 +1,28 @@
+function issueCard(name, age) {
+    var card = {
+        name, 
+        age,
+        numBooksCheckedOut: 0,
+        isChild: false
+    }
+    if (age < 12) {
+     card.isChild = true
+    }
+     return card
+}
 
+function searchByAuthor(collection, author) {
+    var searchResult = []
+    for (var i = 0; i < collection.length; i++) {
+        if (collection[i].author === author) {
+            searchResult.push(collection[i])
+        }
+    } if (searchResult.length < 1) {
+        return 'No book found for search criteria'
+    } else return searchResult
+}
 
-module.exports = { };
+module.exports = {
+    issueCard,
+    searchByAuthor
+ };
